49d011cc4eea954471b00b340977c1efda5cbece
cliftonmaurer <maurerclifton@gmail.com>
2024-05-23 10:58:01 -0500

    Deployed to AWS and tested endpoint functionality

--- app/src/main/java/com/clifm/se/capstone/aquariumbuddy/lambda/tanksLambda/GetTanksLambda.java
+++ app/src/main/java/com/clifm/se/capstone/aquariumbuddy/lambda/tanksLambda/GetTanksLambda.java
@@ -2,6 +2,7 @@ package com.clifm.se.capstone.aquariumbuddy.lambda.tanksLambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 import com.clifm.se.capstone.aquariumbuddy.lambda.LambdaRequest;
 import com.clifm.se.capstone.aquariumbuddy.lambda.LambdaResponse;
 
@@ -9,6 +10,6 @@ public class GetTanksLambda implements RequestHandler<LambdaRequest<String>, Lam
 
     @Override
     public LambdaResponse handleRequest(LambdaRequest<String> input, Context context) {
-        return LambdaResponse.success();
+        return LambdaResponse.success("words");
     }
 }
